6398c79b400c4833c050cc39545908b52ff62b60
JaroxCraft <jarox@jarox.de>
2025-01-02 12:57:13 +0100

    feat: add example plugin from Paplin readme

--- src/main/kotlin/de/jarox/paplin/example/ExamplePlugin.kt
+++ src/main/kotlin/de/jarox/paplin/example/ExamplePlugin.kt
@@ -1,10 +1,36 @@
 package de.jarox.paplin.example
 
 import de.jarox.paplin.PaplinPlugin
+import de.jarox.paplin.chat.component
+import de.jarox.paplin.command.argument
+import de.jarox.paplin.command.command
+import de.jarox.paplin.command.runs
+import de.jarox.paplin.event.listen
+import de.jarox.paplin.extension.broadcast
+import net.kyori.adventure.text.Component
+import org.bukkit.event.block.BlockBreakEvent
 
-@Suppress("unused")
 class ExamplePlugin : PaplinPlugin() {
+
     override fun enable() {
-        logger.info("Hello, Paplin!")
+        // register a simple command
+        command("mycommand") {
+            runs {
+                // automatically only allow players to execute this command
+                player.sendMessage(Component.text("Hello, world!"))
+            }
+
+            // simple argument
+            argument<String>("message") {
+                runs {
+                    this.source.sender.sendMessage(component(getArgument("message")))
+                }
+            }
+        }
+
+        // register a simple listener
+        listen<BlockBreakEvent> {
+            broadcast(Component.text("${it.player.name} broke a block!"))
+        }
     }
 }
